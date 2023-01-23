3e5696b2c8adaedd3628048f452bfa97367858e0
Jscorz <jkscorzafava@gmail.com>
2023-01-22 22:14:38 -0700

    added burtons font into tailwind config and used font in h1 in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 const Navbar = () => {
 	return (
 		<nav className='py-10 mb-12 flex justify-between'>
-			<h1 className='text-xl'>JScorzDev</h1>
+			<h1 className='text-xl font-burtons'>JScorzDev</h1>
 			<ul className='flex items-center'>
 				<li>
 					<BsFillMoonStarsFill className='cursor-pointer text-2xl' />
