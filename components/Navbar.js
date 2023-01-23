9af29286fb3858c8c279d756adf300593db9f73f
Jscorz <jkscorzafava@gmail.com>
2023-01-22 17:59:07 -0700

    added min-h-screen to navbar and edited tailwind config to next js setup as  in tailwindcss docs

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-	return <section className='bg-blue-700'>Navbar</section>;
+	return (
+		<section className='bg-blue-700 min-h-screen'>
+			<h1>Navbar component</h1>
+		</section>
+	);
 };
 
 export default Navbar;
