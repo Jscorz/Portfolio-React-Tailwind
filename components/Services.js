c998fc6548da09ad61e3efd03b227b40ece832aa
Jscorz <jkscorzafava@gmail.com>
2023-01-24 10:45:13 -0700

    created new component for Services and added text to describe services

---
+++ components/Services.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Services = () => {
+	return (
+		<section>
+			<div>
+				<h3 className='text-3xl pb-1 pt-8'>Services I offer</h3>
+				<p className='text-md py-2 leading-8 text-gray-800'>
+					Since the beginning of my journey as a web developer/designer, I've done remote
+					work for
+					<span className='text-teal-500'> agencies</span> consulted for{" "}
+					<span className='text-teal-500'>local businesses</span> and collaborated with
+					talented colleagues to create beautiful websites and mobile applications for
+					both business and consumer use.
+				</p>
+				<p className='text-md py-2 leading-8 text-gray-800'>
+					I offer a multitude of services, including programming for both web and mobile
+					development.
+				</p>
+			</div>
+		</section>
+	);
+};
+
+export default Services;
