5e08b99a9ec96386a978bf93a6e77df1787c9e00
Jscorz <jkscorzafava@gmail.com>
2023-01-24 20:46:31 -0700

    added flex to card container div on md screens and gap-10 to display them in a row, also added max-w-md to p text to ensure cards stay same size

--- components/Card.js
+++ components/Card.js
@@ -5,11 +5,11 @@ import consulting from "../public/consulting.png";
 
 const Card = () => {
 	return (
-		<div>
+		<div className='lg:flex gap-10'>
 			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center'>
 				<Image src={design} height={100} width={100} />
 				<h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-				<p className='py-2'>Creating elegant designs suited for your design theory.</p>
+				<p className='py-2 md:max-w-md'>Creating elegant designs suited for your design theory.</p>
 				<h4 className='py-4 text-teal-600'>Design tools I use</h4>
 				<p className='text-gray-800 py-1'>Figma</p>
 				<p className='text-gray-800 py-1'>Photoshop</p>
@@ -18,7 +18,25 @@ const Card = () => {
 			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center'>
 				<Image src={code} height={100} width={100} />
 				<h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
-				<p className='py-2'>
+				<p className='py-2 md:max-w-md'>
+					Crafting a plan with clients to bring beautiful websites and mobile applications
+					to life.
+				</p>
+				<h4 className='py-4 text-teal-600'>Programming technologies I use</h4>
+				<p className='text-gray-800 py-1'>HTML</p>
+				<p className='text-gray-800 py-1'>CSS</p>
+				<p className='text-gray-800 py-1'>Javascript</p>
+				<p className='text-gray-800 py-1'>React</p>
+				<p className='text-gray-800 py-1'>Next</p>
+				<p className='text-gray-800 py-1'>TailwindCSS</p>
+				<p className='text-gray-800 py-1'>Node</p>
+				<p className='text-gray-800 py-1'>Express</p>
+				<p className='text-gray-800 py-1'>NoSQL(MongoDB)</p>
+			</div>
+			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center'>
+				<Image src={consulting} height={100} width={100} />
+				<h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
+				<p className='py-2 md:max-w-md'>
 					Crafting a plan with clients to bring beautiful websites and mobile applications
 					to life.
 				</p>
