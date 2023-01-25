35a3ab2bf18ab0460783999a37acdfdedf257da1
Jscorz <jkscorzafava@gmail.com>
2023-01-24 20:37:16 -0700

    upped sizing of fonts for md and lg screens and increased avatar image size as well

--- components/Hero.js
+++ components/Hero.js
@@ -7,9 +7,9 @@ const Hero = () => {
 	return (
 		<div>
 			<div className='text-center p-10'>
-				<h2 className='text-5xl py-2 text-teal-600 font-medium'>Justin Scorzafava</h2>
-				<h3 className='text-2xl py-2'>Developer and designer.</h3>
-				<p className='text-md py-5 leading-8 text-gray-800'>
+				<h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Justin Scorzafava</h2>
+				<h3 className='text-2xl py-2 md:text-3xl'>Developer and designer.</h3>
+				<p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>
 					Web Developer providing services for programming and design content needs. Join
 					me below and let's get to it!
 				</p>
@@ -19,7 +19,7 @@ const Hero = () => {
 				<AiFillLinkedin />
 				<AiFillYoutube />
 			</div>
-			<div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+			<div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
 				<Image src={justindev} layout='fill' objectFit='cover' />
 			</div>
 		</div>
