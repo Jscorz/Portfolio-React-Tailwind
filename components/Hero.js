7b907fcf5154bc33135154a7c6544798f4006078
Jscorz <jkscorzafava@gmail.com>
2023-01-25 21:23:42 -0700

    installed react-simple-typewriter to utilize typewriter animation effect for header underneath name in hero section, changed h3 to a span and wrapped that and the Cursor component inside the h3 parent tag

--- components/Hero.js
+++ components/Hero.js
@@ -3,8 +3,15 @@ import Image from "next/image";
 // import justindev from "../public/avatar.png";
 // import justindev from "../public/avatars.png";
 import justindev from "../public/avatars-transparent.png";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 const Hero = () => {
+	const [text, count] = useTypewriter({
+		words: ["<Developer />", "<Designer />", "Guy-Who-Loves-Coffee.jsx"],
+		loop: true,
+		delaySpeed: 2000,
+	});
+
 	return (
 		<div>
 			<div className='text-center p-10'>
@@ -12,8 +19,10 @@ const Hero = () => {
 					Justin Scorzafava
 				</h2>
 				<h3 className='text-2xl py-2 md:text-3xl  dark:text-gray-100'>
-					Developer and designer.
+					<span>{text}</span>
+					<Cursor cursorColor='#F7ABBA' />
 				</h3>
+
 				<p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>
 					Web Developer providing services for programming and design content needs. Join
 					me below and let's get to it!
