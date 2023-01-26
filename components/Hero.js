36fbb6dd556b3601a44bdd0d85819860f8a883ba
Jscorz <jkscorzafava@gmail.com>
2023-01-25 21:55:17 -0700

    added delayHasPassed state value and useEffect to setDelayHasPassed to true after 2000 ms, then conditionally rendered the typewriter text after delayHasPassed and used framer motion to animate from opacity 0 to 1

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,24 @@
+import { useState, useEffect } from "react";
 import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import Image from "next/image";
 // import justindev from "../public/avatar.png";
 // import justindev from "../public/avatars.png";
 import justindev from "../public/avatars-transparent.png";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+	const [delayHasPassed, setDelayHasPassed] = useState(false);
 	const [text, count] = useTypewriter({
 		words: ["<Developer />", "<Designer />", "Guy-Who-Loves-Coffee.jsx"],
 		loop: true,
 		delaySpeed: 2000,
+	});
+
+	useEffect(() => {
+		setTimeout(() => {
+			setDelayHasPassed(true);
+		}, 2000);
 	});
 
 	return (
@@ -18,10 +27,20 @@ const Hero = () => {
 				<h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
 					Justin Scorzafava
 				</h2>
-				<h3 className='text-2xl py-2 md:text-3xl  dark:text-gray-100'>
-					<span>{text}</span>
-					<Cursor cursorColor='#F7ABBA' />
-				</h3>
+
+				<div className={delayHasPassed ? "" : "p-[1.65rem]"}>
+					{delayHasPassed && (
+						<motion.h3
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							transition={{ ease: "easeOut", duration: 2 }}
+							className='text-2xl py-2 md:text-3xl  dark:text-gray-100'
+						>
+							<span>{text}</span>
+							<Cursor cursorColor='#F7ABBA' />
+						</motion.h3>
+					)}
+				</div>
 
 				<p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>
 					Web Developer providing services for programming and design content needs. Join
