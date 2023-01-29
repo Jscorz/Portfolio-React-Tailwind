f715564d82ae5e00f5a25880ec55869a221525ce
Jscorz <jkscorzafava@gmail.com>
2023-01-28 18:12:55 -0700

    added sun icon for light/dark mode switcher to change which icon is displayed and added hover effects to each

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import { motion } from "framer-motion";
 
-const Navbar = ({ toggleDarkMode }) => {
+const Navbar = ({ darkMode, toggleDarkMode }) => {
 	return (
 		<nav className='py-10 mb-12 flex justify-between'>
 			<motion.h1
@@ -20,12 +20,22 @@ const Navbar = ({ toggleDarkMode }) => {
 				transition={{ duration: 1.5 }}
 				className='flex items-center'
 			>
-				<li>
-					<BsFillMoonStarsFill
-						onClick={toggleDarkMode}
-						className='cursor-pointer text-2xl dark:text-gray-200'
-					/>
-				</li>
+				{!darkMode && (
+					<li>
+						<BsFillMoonStarsFill
+							onClick={toggleDarkMode}
+							className='cursor-pointer text-2xl dark:text-gray-200 hover:text-gray-400 transition-all'
+						/>
+					</li>
+				)}
+				{darkMode && (
+					<li>
+						<BsFillSunFill
+							onClick={toggleDarkMode}
+							className='cursor-pointer text-3xl dark:text-gray-200 dark:hover:text-yellow-400 transition-all'
+						/>
+					</li>
+				)}
 				<li>
 					<a
 						className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
