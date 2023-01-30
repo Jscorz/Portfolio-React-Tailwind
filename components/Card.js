13e0c87e7115c0834efe1f99f6e88aa442f0dba9
Jscorz <jkscorzafava@gmail.com>
2023-01-30 09:45:46 -0700

    added framer motion animation to card elements in service section, with initial={{opacity:0}} and whileInView={{opacity:1}} with a duration of 1.0, might change that later as it may be a bit slow

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,19 @@ import Image from "next/image";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
+import { motion } from "framer-motion";
 
 const Card = () => {
 	return (
 		<div className='lg:flex gap-10'>
-			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'>
+			<motion.div
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				whileHover={{ scale: 1.1 }}
+				transition={{ duration: 1.0, type: "tween" }}
+				key={1}
+				className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'
+			>
 				<Image src={design} height={100} width={100} />
 				<h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
 				<p className='py-2 md:max-w-md'>
@@ -16,8 +24,15 @@ const Card = () => {
 				<p className='text-gray-800 py-1'>Figma</p>
 				<p className='text-gray-800 py-1'>Photoshop</p>
 				<p className='text-gray-800 py-1'>Illustrator</p>
-			</div>
-			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'>
+			</motion.div>
+			<motion.div
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				whileHover={{ scale: 1.1 }}
+				transition={{ duration: 1.0, type: "tween" }}
+				key={2}
+				className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'
+			>
 				<Image src={code} height={100} width={100} />
 				<h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
 				<p className='py-2 md:max-w-md'>
@@ -34,8 +49,15 @@ const Card = () => {
 				<p className='text-gray-800 py-1'>Node</p>
 				<p className='text-gray-800 py-1'>Express</p>
 				<p className='text-gray-800 py-1'>NoSQL(MongoDB)</p>
-			</div>
-			<div className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'>
+			</motion.div>
+			<motion.div
+				initial={{ opacity: 0 }}
+				whileInView={{ opacity: 1 }}
+				whileHover={{ scale: 1.1 }}
+				transition={{ duration: 1.0, type: "tween" }}
+				key={3}
+				className='text-center shadow-lg p-10 rounded-xl my-10 flex flex-col items-center dark:bg-white'
+			>
 				<Image src={consulting} height={100} width={100} />
 				<h3 className='text-lg font-medium pt-8 pb-2'>Consulting</h3>
 				<p className='py-2 md:max-w-md'>
@@ -52,7 +74,7 @@ const Card = () => {
 				<p className='text-gray-800 py-1'>Node</p>
 				<p className='text-gray-800 py-1'>Express</p>
 				<p className='text-gray-800 py-1'>NoSQL(MongoDB)</p>
-			</div>
+			</motion.div>
 		</div>
 	);
 };
