8637399b91f91fd7508f94f331f5aeb92f180ba3
Jscorz <jkscorzafava@gmail.com>
2023-02-04 20:36:28 -0700

    added linear gradient to company title section same gradient as resume button in navbar/also added styles for dark mode

--- components/Experience.js
+++ components/Experience.js
@@ -3,29 +3,29 @@ import { FaAngleDoubleRight, FaLongArrowAltRight } from "react-icons/fa";
 
 const Experience = () => {
 	return (
-		<section className='flex flex-col space-y-3 pt-8 pb-28'>
+		<section className='flex flex-col space-y-4 pt-8 pb-28 md:space-y-6'>
 			<div className='flex items-center py-8'>
-				<h1 className='text-gray-900 text-4xl font-bold border-b-2 border-b-teal-700'>
+				<h1 className='text-gray-900 text-4xl font-bold border-b-2 border-b-teal-700 dark:text-[#f1f1f1]'>
 					Experience
 				</h1>
 			</div>
-			<h1 className='text-2xl'>Frontend Developer</h1>
+			<h1 className='text-2xl dark:text-[#f1f1f1]'>Frontend Developer</h1>
 			<div>
-				<button className='bg-gray-300 text-gray-600 font-bold p-2 rounded-md'>
+				<span className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white font-bold p-2 rounded-md'>
 					WOMANSDIVORCE
-				</button>
+				</span>
 			</div>
-			<p className='text-gray-700 text-sm'>February/2022 - Present</p>
+			<p className='text-gray-700 text-sm dark:text-gray-200'>February/2022 - Present</p>
 			<div className='grid grid-cols-10 items-center text-gray-800 py-1'>
 				<FaAngleDoubleRight className='text-teal-600 col-start-1 col-span-1'></FaAngleDoubleRight>
-				<p className='col-span-9 text-gray-700'>
+				<p className='col-span-9 text-gray-700 dark:text-gray-200'>
 					Revolutionzed Frontend operations by boosting website perforamnce by a
 					substantial 40% through strategic improvements to the Frontend stack
 				</p>
 			</div>
 			<div className='grid grid-cols-10 items-center text-gray-800 py-1'>
 				<FaAngleDoubleRight className='text-teal-600 col-start-1 col-span-1'></FaAngleDoubleRight>
-				<p className='col-span-9 text-gray-700'>
+				<p className='col-span-9 text-gray-700 dark:text-gray-200'>
 					Leveraged expertise in responsive web design to craft visually stunning and
 					user-friendly websites resulting in increased client acquisition and a boosted
 					sales pipeline
@@ -33,7 +33,7 @@ const Experience = () => {
 			</div>
 			<div className='grid grid-cols-10 items-center text-gray-800 py-1'>
 				<FaAngleDoubleRight className='text-teal-600 col-start-1 col-span-1'></FaAngleDoubleRight>
-				<p className='col-span-9 text-gray-700'>
+				<p className='col-span-9 text-gray-700 dark:text-gray-200'>
 					Optimized clients' web presence and drove organic traffic growth through expert
 					implementation of SEO strategies, strategic keyword optimization, and
 					utilization of React Helmet
@@ -41,7 +41,7 @@ const Experience = () => {
 			</div>
 			<div className='grid grid-cols-10 items-center text-gray-800 py-1'>
 				<FaAngleDoubleRight className='text-teal-600 col-start-1 col-span-1'></FaAngleDoubleRight>
-				<p className='col-span-9 text-gray-700'>
+				<p className='col-span-9 text-gray-700 dark:text-gray-200'>
 					Crafted compelling proposals and meticulously developed project schedules for
 					multiple website development and maintenance intiatives, ensuring seamless
 					execution and delivery
