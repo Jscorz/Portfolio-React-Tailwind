5c74ab7bde39a4fed936ba85bb492ecd93382b47
Jscorz <jkscorzafava@gmail.com>
2023-02-04 23:07:23 -0700

    adjusted scale value on hover to be a bit lower 1.05 on desktop

--- components/PortfolioWorks.js
+++ components/PortfolioWorks.js
@@ -23,7 +23,7 @@ const PortfolioCard = () => {
 		: {
 				initial: { opacity: 0, scale: 0, y: -10 },
 				whileInView: { opacity: [0, 1], scale: 1, y: 0 },
-				whileHover: { scale: [1, 1.1] },
+				whileHover: { scale: [1, 1.05] },
 				transition: { duration: 0.5 },
 		  };
 
