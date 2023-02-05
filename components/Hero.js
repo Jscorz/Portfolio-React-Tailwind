e3a0ae8af53bb0a41030f9c31cbc565e69ddee60
Jscorz <jkscorzafava@gmail.com>
2023-02-04 23:00:18 -0700

    raised intial hero image up a bit as to be in view in original screen view and adjusted padding of other elements to even changes out

--- components/Hero.js
+++ components/Hero.js
@@ -22,7 +22,7 @@ const Hero = () => {
 
 	return (
 		<div>
-			<div className='text-center p-10'>
+			<div className='text-center px-10'>
 				<h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
 					Justin Scorzafava
 				</h2>
@@ -36,17 +36,17 @@ const Hero = () => {
 							className='text-2xl py-2 md:text-3xl  dark:text-gray-100'
 						>
 							<span>{text}</span>
-							<Cursor cursorColor='#F7ABBA' />
+							<Cursor cursorColor='#00897B' />
 						</motion.h3>
 					)}
 				</div>
 
-				<p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>
+				<p className='text-md pt-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-200'>
 					I'm a Web Developer providing services for programming and design content needs.
 					Join me below and let's get to it!
 				</p>
 			</div>
-			<div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+			<div className='text-5xl flex justify-center gap-16 py-6 text-gray-600'>
 				<a
 					className='hover:text-teal-600 transition-all'
 					target='_blank'
@@ -63,7 +63,7 @@ const Hero = () => {
 				</a>
 				{/* <AiFillMail /> */}
 			</div>
-			<div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+			<div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden md:h-96 md:w-96'>
 				<Image src={justindev} />
 			</div>
 		</div>
